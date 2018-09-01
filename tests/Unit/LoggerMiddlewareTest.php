0fa0f7d6dea4533678f06eb2d3957cf2bebe68ac
George Mponos <gmponos@gmail.com>
2018-09-01 13:46:08 +0300

    Update tests to use constrants as LogLevels

--- tests/Unit/LoggerMiddlewareTest.php
+++ tests/Unit/LoggerMiddlewareTest.php
@@ -11,8 +11,9 @@ use GuzzleHttp\Handler\MockHandler;
 use GuzzleHttp\HandlerStack;
 use GuzzleHttp\Psr7\Response;
 use GuzzleHttp\RequestOptions;
+use Psr\Log\LogLevel;
 
-class LoggerMiddlewareTest extends \PHPUnit_Framework_TestCase
+final class LoggerMiddlewareTest extends \PHPUnit_Framework_TestCase
 {
     /**
      * @var MockHandler
@@ -52,7 +53,7 @@ class LoggerMiddlewareTest extends \PHPUnit_Framework_TestCase
      * @param array $options
      * @return Client
      */
-    private function getClient($options = [])
+    private function createClient(array $options = [])
     {
         $stack = HandlerStack::create($this->mockHandler);
         $stack->unshift(new LoggerMiddleware($this->logger));
@@ -69,14 +70,14 @@ class LoggerMiddlewareTest extends \PHPUnit_Framework_TestCase
     public function logSuccessfulTransaction()
     {
         $this->appendResponse(200, [], 'response_body')
-            ->getClient()
+            ->createClient()
             ->get('/', [RequestOptions::BODY => 'request_body']);
 
         $this->assertCount(2, $this->logger->history);
-        $this->assertSame('debug', $this->logger->history[0]['level']);
+        $this->assertSame(LogLevel::DEBUG, $this->logger->history[0]['level']);
         $this->assertSame('Guzzle HTTP request', $this->logger->history[0]['message']);
         $this->assertSame('request_body', $this->logger->history[0]['context']['request']['body']);
-        $this->assertSame('debug', $this->logger->history[1]['level']);
+        $this->assertSame(LogLevel::DEBUG, $this->logger->history[1]['level']);
         $this->assertSame('Guzzle HTTP response', $this->logger->history[1]['message']);
         $this->assertSame('response_body', $this->logger->history[1]['context']['response']['body']);
     }
@@ -87,7 +88,7 @@ class LoggerMiddlewareTest extends \PHPUnit_Framework_TestCase
     public function doNotLogSuccessfulTransaction()
     {
         $this->appendResponse(200)
-            ->getClient([
+            ->createClient([
                 'log' => [
                     'requests' => false,
                 ],
@@ -106,7 +107,7 @@ class LoggerMiddlewareTest extends \PHPUnit_Framework_TestCase
             $this
                 ->appendResponse(200)
                 ->appendResponse(500);
-            $client = $this->getClient([
+            $client = $this->createClient([
                 'log' => [
                     'requests' => false,
                 ],
@@ -117,9 +118,9 @@ class LoggerMiddlewareTest extends \PHPUnit_Framework_TestCase
         }
 
         $this->assertCount(2, $this->logger->history);
-        $this->assertSame('debug', $this->logger->history[0]['level']);
+        $this->assertSame(LogLevel::DEBUG, $this->logger->history[0]['level']);
         $this->assertSame('Guzzle HTTP request', $this->logger->history[0]['message']);
-        $this->assertSame('critical', $this->logger->history[1]['level']);
+        $this->assertSame(LogLevel::CRITICAL, $this->logger->history[1]['level']);
         $this->assertSame('Guzzle HTTP response', $this->logger->history[1]['message']);
     }
 
@@ -129,13 +130,13 @@ class LoggerMiddlewareTest extends \PHPUnit_Framework_TestCase
     public function logTransactionWith4xxCode()
     {
         try {
-            $this->appendResponse(404)->getClient()->get('/');
+            $this->appendResponse(404)->createClient()->get('/');
         } catch (RequestException $e) {
         }
         $this->assertCount(2, $this->logger->history);
-        $this->assertSame('debug', $this->logger->history[0]['level']);
+        $this->assertSame(LogLevel::DEBUG, $this->logger->history[0]['level']);
         $this->assertSame('Guzzle HTTP request', $this->logger->history[0]['message']);
-        $this->assertSame('error', $this->logger->history[1]['level']);
+        $this->assertSame(LogLevel::ERROR, $this->logger->history[1]['level']);
         $this->assertSame('Guzzle HTTP response', $this->logger->history[1]['message']);
     }
 
@@ -145,7 +146,7 @@ class LoggerMiddlewareTest extends \PHPUnit_Framework_TestCase
     public function logTransactionWith5xxCode()
     {
         try {
-            $this->appendResponse(500)->getClient()->get('/');
+            $this->appendResponse(500)->createClient()->get('/');
         } catch (RequestException $e) {
         }
         $this->assertCount(2, $this->logger->history);
@@ -160,11 +161,11 @@ class LoggerMiddlewareTest extends \PHPUnit_Framework_TestCase
      */
     public function logTransactionWith4xxCodeWithoutExceptions()
     {
-        $this->appendResponse(404)->getClient(['exceptions' => false])->get('/');
+        $this->appendResponse(404)->createClient(['exceptions' => false])->get('/');
         $this->assertCount(2, $this->logger->history);
-        $this->assertSame('debug', $this->logger->history[0]['level']);
+        $this->assertSame(LogLevel::DEBUG, $this->logger->history[0]['level']);
         $this->assertSame('Guzzle HTTP request', $this->logger->history[0]['message']);
-        $this->assertSame('error', $this->logger->history[1]['level']);
+        $this->assertSame(LogLevel::ERROR, $this->logger->history[1]['level']);
         $this->assertSame('Guzzle HTTP response', $this->logger->history[1]['message']);
     }
 
@@ -173,11 +174,11 @@ class LoggerMiddlewareTest extends \PHPUnit_Framework_TestCase
      */
     public function logTransactionWith5xxCodeWithoutExceptions()
     {
-        $this->appendResponse(500)->getClient(['exceptions' => false])->get('/');
+        $this->appendResponse(500)->createClient(['exceptions' => false])->get('/');
         $this->assertCount(2, $this->logger->history);
-        $this->assertSame('debug', $this->logger->history[0]['level']);
+        $this->assertSame(LogLevel::DEBUG, $this->logger->history[0]['level']);
         $this->assertSame('Guzzle HTTP request', $this->logger->history[0]['message']);
-        $this->assertSame('critical', $this->logger->history[1]['level']);
+        $this->assertSame(LogLevel::CRITICAL, $this->logger->history[1]['level']);
         $this->assertSame('Guzzle HTTP response', $this->logger->history[1]['message']);
     }
 
@@ -190,13 +191,13 @@ class LoggerMiddlewareTest extends \PHPUnit_Framework_TestCase
             'Content-Type' => 'application/json',
         ];
         $this->appendResponse(200, $headers, '{"status": true, "client": 13000}')
-            ->getClient(['exceptions' => false])
+            ->createClient(['exceptions' => false])
             ->get('/');
 
         $this->assertCount(2, $this->logger->history);
-        $this->assertSame('debug', $this->logger->history[0]['level']);
+        $this->assertSame(LogLevel::DEBUG, $this->logger->history[0]['level']);
         $this->assertSame('Guzzle HTTP request', $this->logger->history[0]['message']);
-        $this->assertSame('debug', $this->logger->history[1]['level']);
+        $this->assertSame(LogLevel::DEBUG, $this->logger->history[1]['level']);
         $this->assertSame('Guzzle HTTP response', $this->logger->history[1]['message']);
         $this->assertContains(
             [
@@ -216,13 +217,13 @@ class LoggerMiddlewareTest extends \PHPUnit_Framework_TestCase
             'Content-Type' => 'application/vnd.api+json',
         ];
         $this->appendResponse(200, $headers, '{"status": true, "client": 13000}')
-            ->getClient(['exceptions' => false])
+            ->createClient(['exceptions' => false])
             ->get('/');
 
         $this->assertCount(2, $this->logger->history);
-        $this->assertSame('debug', $this->logger->history[0]['level']);
+        $this->assertSame(LogLevel::DEBUG, $this->logger->history[0]['level']);
         $this->assertSame('Guzzle HTTP request', $this->logger->history[0]['message']);
-        $this->assertSame('debug', $this->logger->history[1]['level']);
+        $this->assertSame(LogLevel::DEBUG, $this->logger->history[1]['level']);
         $this->assertSame('Guzzle HTTP response', $this->logger->history[1]['message']);
         $this->assertContains(
             [
@@ -241,15 +242,14 @@ class LoggerMiddlewareTest extends \PHPUnit_Framework_TestCase
         try {
             $this->mockHandler->append(new TransferException());
 
-            $this->getClient()
-                ->get('/');
+            $this->createClient()->get('/');
         } catch (\Exception $e) {
         }
 
         $this->assertCount(2, $this->logger->history);
-        $this->assertSame('debug', $this->logger->history[0]['level']);
+        $this->assertSame(LogLevel::DEBUG, $this->logger->history[0]['level']);
         $this->assertSame('Guzzle HTTP request', $this->logger->history[0]['message']);
-        $this->assertSame('critical', $this->logger->history[1]['level']);
+        $this->assertSame(LogLevel::CRITICAL, $this->logger->history[1]['level']);
         $this->assertSame('Guzzle HTTP exception', $this->logger->history[1]['message']);
     }
 
@@ -260,7 +260,7 @@ class LoggerMiddlewareTest extends \PHPUnit_Framework_TestCase
     {
         try {
             $this->appendResponse(404)
-                ->getClient([
+                ->createClient([
                     'log' => [
                         'warning_threshold' => null,
                     ],
@@ -271,7 +271,7 @@ class LoggerMiddlewareTest extends \PHPUnit_Framework_TestCase
 
         try {
             $this->appendResponse(500)
-                ->getClient([
+                ->createClient([
                     'log' => [
                         'warning_threshold' => null,
                     ],
@@ -281,14 +281,14 @@ class LoggerMiddlewareTest extends \PHPUnit_Framework_TestCase
         }
 
         $this->assertCount(4, $this->logger->history);
-        $this->assertSame('debug', $this->logger->history[0]['level']);
+        $this->assertSame(LogLevel::DEBUG, $this->logger->history[0]['level']);
         $this->assertSame('Guzzle HTTP request', $this->logger->history[0]['message']);
-        $this->assertSame('debug', $this->logger->history[1]['level']);
+        $this->assertSame(LogLevel::DEBUG, $this->logger->history[1]['level']);
         $this->assertSame('Guzzle HTTP response', $this->logger->history[1]['message']);
 
-        $this->assertSame('debug', $this->logger->history[2]['level']);
+        $this->assertSame(LogLevel::DEBUG, $this->logger->history[2]['level']);
         $this->assertSame('Guzzle HTTP request', $this->logger->history[2]['message']);
-        $this->assertSame('critical', $this->logger->history[3]['level']);
+        $this->assertSame(LogLevel::CRITICAL, $this->logger->history[3]['level']);
         $this->assertSame('Guzzle HTTP response', $this->logger->history[3]['message']);
     }
 
@@ -299,7 +299,7 @@ class LoggerMiddlewareTest extends \PHPUnit_Framework_TestCase
     {
         try {
             $this->appendResponse(404)
-                ->getClient([
+                ->createClient([
                     'log' => [
                         'warning_threshold' => null,
                         'error_threshold' => null,
@@ -311,7 +311,7 @@ class LoggerMiddlewareTest extends \PHPUnit_Framework_TestCase
 
         try {
             $this->appendResponse(500)
-                ->getClient([
+                ->createClient([
                     'log' => [
                         'warning_threshold' => null,
                         'error_threshold' => null,
@@ -322,14 +322,14 @@ class LoggerMiddlewareTest extends \PHPUnit_Framework_TestCase
         }
 
         $this->assertCount(4, $this->logger->history);
-        $this->assertSame('debug', $this->logger->history[0]['level']);
+        $this->assertSame(LogLevel::DEBUG, $this->logger->history[0]['level']);
         $this->assertSame('Guzzle HTTP request', $this->logger->history[0]['message']);
-        $this->assertSame('debug', $this->logger->history[1]['level']);
+        $this->assertSame(LogLevel::DEBUG, $this->logger->history[1]['level']);
         $this->assertSame('Guzzle HTTP response', $this->logger->history[1]['message']);
 
-        $this->assertSame('debug', $this->logger->history[2]['level']);
+        $this->assertSame(LogLevel::DEBUG, $this->logger->history[2]['level']);
         $this->assertSame('Guzzle HTTP request', $this->logger->history[2]['message']);
-        $this->assertSame('debug', $this->logger->history[3]['level']);
+        $this->assertSame(LogLevel::DEBUG, $this->logger->history[3]['level']);
         $this->assertSame('Guzzle HTTP response', $this->logger->history[3]['message']);
     }
 
@@ -339,7 +339,7 @@ class LoggerMiddlewareTest extends \PHPUnit_Framework_TestCase
     public function logTransactionWithStatistics()
     {
         $this->appendResponse(200)
-            ->getClient([
+            ->createClient([
                 'log' => [
                     'statistics' => true,
                 ],
@@ -347,11 +347,11 @@ class LoggerMiddlewareTest extends \PHPUnit_Framework_TestCase
             ->get('/');
 
         $this->assertCount(3, $this->logger->history);
-        $this->assertSame('debug', $this->logger->history[0]['level']);
+        $this->assertSame(LogLevel::DEBUG, $this->logger->history[0]['level']);
         $this->assertSame('Guzzle HTTP request', $this->logger->history[0]['message']);
-        $this->assertSame('debug', $this->logger->history[1]['level']);
+        $this->assertSame(LogLevel::DEBUG, $this->logger->history[1]['level']);
         $this->assertSame('Guzzle HTTP statistics', $this->logger->history[1]['message']);
-        $this->assertSame('debug', $this->logger->history[2]['level']);
+        $this->assertSame(LogLevel::DEBUG, $this->logger->history[2]['level']);
         $this->assertSame('Guzzle HTTP response', $this->logger->history[2]['message']);
     }
 
@@ -361,7 +361,7 @@ class LoggerMiddlewareTest extends \PHPUnit_Framework_TestCase
     public function logTransactionWithCustomLevel()
     {
         $this->appendResponse(300)
-            ->getClient([
+            ->createClient([
                 'log' => [
                     'levels' => [
                         300 => 'warning',
@@ -375,9 +375,9 @@ class LoggerMiddlewareTest extends \PHPUnit_Framework_TestCase
             ->get('/');
 
         $this->assertCount(2, $this->logger->history);
-        $this->assertSame('debug', $this->logger->history[0]['level']);
+        $this->assertSame(LogLevel::DEBUG, $this->logger->history[0]['level']);
         $this->assertSame('Guzzle HTTP request', $this->logger->history[0]['message']);
-        $this->assertSame('warning', $this->logger->history[1]['level']);
+        $this->assertSame(LogLevel::WARNING, $this->logger->history[1]['level']);
         $this->assertSame('Guzzle HTTP response', $this->logger->history[1]['message']);
     }
 
@@ -387,7 +387,7 @@ class LoggerMiddlewareTest extends \PHPUnit_Framework_TestCase
     public function doNotRequestOrResponseBodyBecauseOfSensitiveData()
     {
         $this->appendResponse(200, [], 'sensitive_data')
-            ->getClient([
+            ->createClient([
                 'log' => [
                     'sensitive' => true,
                 ],
@@ -395,10 +395,10 @@ class LoggerMiddlewareTest extends \PHPUnit_Framework_TestCase
             ->get('/', [RequestOptions::BODY => 'sensitive_request_data']);
 
         $this->assertCount(2, $this->logger->history);
-        $this->assertSame('debug', $this->logger->history[0]['level']);
+        $this->assertSame(LogLevel::DEBUG, $this->logger->history[0]['level']);
         $this->assertSame('Guzzle HTTP request', $this->logger->history[0]['message']);
         $this->assertSame('Body contains sensitive information therefore it is not included.', $this->logger->history[0]['context']['request']['body']);
-        $this->assertSame('debug', $this->logger->history[1]['level']);
+        $this->assertSame(LogLevel::DEBUG, $this->logger->history[1]['level']);
         $this->assertSame('Guzzle HTTP response', $this->logger->history[1]['message']);
         $this->assertSame('Body contains sensitive information therefore it is not included.', $this->logger->history[1]['context']['response']['body']);
     }
@@ -437,13 +437,13 @@ class LoggerMiddlewareTest extends \PHPUnit_Framework_TestCase
             'Very big body..Very big body..Very big body..Very big body..Very big body..Very big body..Very big body..Very big body..Very big body..';
 
         $this->appendResponse(300, [], $body)
-            ->getClient()
+            ->createClient()
             ->get('/');
 
         $this->assertCount(2, $this->logger->history);
-        $this->assertSame('debug', $this->logger->history[0]['level']);
+        $this->assertSame(LogLevel::DEBUG, $this->logger->history[0]['level']);
         $this->assertSame('Guzzle HTTP request', $this->logger->history[0]['message']);
-        $this->assertSame('debug', $this->logger->history[1]['level']);
+        $this->assertSame(LogLevel::DEBUG, $this->logger->history[1]['level']);
         $this->assertSame('Guzzle HTTP response', $this->logger->history[1]['message']);
         $this->assertStringEndsWith(' (truncated...)', $this->logger->history[1]['context']['response']['body']);
     }
